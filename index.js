548bc9ce43a34217a4446f64bbef8ce557d10ac1
hrshihab <hrshihab10@gmail.com>
2024-11-29 04:05:47 +0600

    done

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 5000;
 app.use(cookieParser());
 
 
-app.use(cors({ origin: 'http://localhost:3000',credentials: true })); // 
+app.use(cors({ origin: 'https://luminedge.netlify.app',credentials: true })); // 
 app.use(express.json());
 
 // MongoDB Connection URL
